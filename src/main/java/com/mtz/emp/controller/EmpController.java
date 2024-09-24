efb2cc868c95018a7be09c1d3eabfa9e1ee36a59
basha-github <sharifabee.alur@gmail.com>
2024-09-24 20:01:34 +0530

    get put delete

--- src/main/java/com/mtz/emp/controller/EmpController.java
+++ src/main/java/com/mtz/emp/controller/EmpController.java
@@ -1,8 +1,14 @@
 package com.mtz.emp.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mtz.emp.model.Employee;
@@ -23,5 +29,29 @@ public class EmpController {
 		
 		
 	}
+	
+	@PutMapping("/mtz/emp/update")
+	public Employee updateEmp(@RequestBody Employee emp){
+		
+		return empService.updateEmp(emp);
+		
+		
+	}
+	@DeleteMapping("/mtz/emp/delete")
+	public String deleteEmp(@RequestParam int id){
+		
+		return empService.deleteEmp(id);
+		
+		
+	}
+	
+	@GetMapping("/mtz/emp/all")
+	public List<Employee> getAllEmps(){
+		
+		return empService.getAllEmps();
+		
+		
+	}
+	
 
 }
--- src/main/java/com/mtz/emp/service/EmpService.java
+++ src/main/java/com/mtz/emp/service/EmpService.java
@@ -1,5 +1,7 @@
 package com.mtz.emp.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,15 +10,53 @@ import com.mtz.emp.repo.EmpRepo;
 
 @Service
 public class EmpService {
-	
+
 	@Autowired
 	EmpRepo empRepo;
 
 	public Employee addNewEmp(Employee emp) {
-		
+
 		return empRepo.save(emp);
+
+	}
+
+	public List<Employee> getAllEmps() {
+		return empRepo.findAll();
+	}
+
+	public Employee updateEmp(Employee emp) {
+
+		Employee dbEmp = null;
+		try {
+			dbEmp = empRepo.findById(emp.getEid()).get();
+			if (dbEmp != null) {
+				return empRepo.save(emp);
+			}
+		} catch (Exception e) {
+
+		}
+
+		return dbEmp;
+
+	}
+
+	public String deleteEmp(int id) {
+		Employee dbEmp = null;
+		String res = "Successfully Deleted record-->"+id;
+		try {
+			dbEmp = empRepo.findById(id).get();
+			if (dbEmp != null) {
+				empRepo.deleteById(id);
+			}
+			else {
+				res = "falied to delete record--->"+id;
+			}
+				
+		}catch(Exception e) {
+			res = "falied to delete record--->"+id;
+		}
 		
-		
+		return res;
 	}
 
 }
